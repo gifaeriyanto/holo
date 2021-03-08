1f9f5afbffc7971439a935510b0bdfa8a44c3238
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-09 00:07:18 +0800

    welcome and guide video

---
+++ pages/display.tsx
@@ -0,0 +1,44 @@
+import { useInterval, Box } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
+import {
+  getVideoPlaying,
+  playGuideVideo,
+  playVideo,
+  playWelcomeVideo,
+} from 'utils/playVideos';
+import { GUIDE_VIDEO, WELCOME_VIDEO } from 'utils/variables';
+
+const Index: NextPage = () => {
+  const [playing, setPlaying] = useState(WELCOME_VIDEO);
+  const [isWelcomingOrGuiding, setIsWelcomingOrGuiding] = useState(true);
+
+  useInterval(() => {
+    const videoUrl = getVideoPlaying();
+    setPlaying(videoUrl);
+    setIsWelcomingOrGuiding([GUIDE_VIDEO, WELCOME_VIDEO].includes(playing));
+  }, 1000);
+
+  const handleEnd = () => {
+    if (!isWelcomingOrGuiding) {
+      playGuideVideo();
+    }
+  };
+
+  return (
+    <Box bgColor="#000" h="100vh">
+      <ReactPlayer
+        url={playing}
+        playing
+        muted
+        onEnded={handleEnd}
+        loop={isWelcomingOrGuiding}
+        width="100%"
+        height="100%"
+      />
+    </Box>
+  );
+};
+
+export default Index;
