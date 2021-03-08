9bae78cbb0b5bc767c48e85c374c47e963ca1381
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-09 03:47:46 +0800

    dashboard page

---
+++ pages/admin/dashboard.tsx
@@ -0,0 +1,16 @@
+import AdminLayout from 'layouts/admin';
+import { NextPage } from 'next';
+import React from 'react';
+
+const Dashboard: NextPage = () => {
+  return (
+    <AdminLayout title="Hello">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis cum
+      quibusdam amet a voluptatibus deleniti cumque nemo officiis dolorem eos,
+      culpa beatae exercitationem voluptates ut quam laborum optio, assumenda
+      quaerat.
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
