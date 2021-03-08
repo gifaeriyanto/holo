a70639cbcce268a80367732a5e2f74649afa14b7
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-09 03:47:13 +0800

    routes utils

---
+++ utils/routes.tsx
@@ -0,0 +1,16 @@
+export const routes = {
+  admin: {
+    dashboard: { url: '/admin/dashboard', name: 'Dashboard' },
+    welcomeAndGuide: {
+      url: '/admin/welcome-and-guide',
+      name: 'Welcome & Guide',
+    },
+    items: { url: '/admin/items', name: 'Items' },
+  },
+  client: {
+    display: { url: '/display', name: 'Display' },
+    home: { url: '/', name: 'Home' },
+    login: { url: '/login', name: 'Login' },
+    main: { url: '/main', name: 'Main' },
+  },
+};
