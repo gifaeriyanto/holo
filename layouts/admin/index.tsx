79c42198c290def41f26e101fc072decb2d16b67
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-13 20:47:22 +0800

    remove unused import

--- layouts/admin/index.tsx
+++ layouts/admin/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Icon } from '@chakra-ui/react';
 import withAuth from 'layouts/admin/hoc/withAuth';
 import { useRouter } from 'next/dist/client/router';
 import Link from 'next/link';
@@ -17,34 +17,59 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({
 }) => {
   const router = useRouter();
 
-  const listOfMenu = Object.entries(routes.admin).map((menu) => (
-    <Link href={menu[1].url} key={menu[1].name}>
-      <Box
-        as="a"
-        p={3}
-        borderRadius="lg"
-        cursor="pointer"
-        bgColor={
-          router.pathname === menu[1].url ? 'rgba(0, 0, 0, 0.2)' : 'transparent'
-        }
-      >
-        {menu[1].name}
-      </Box>
-    </Link>
-  ));
+  const listOfMenu = Object.entries(routes.admin)
+    .filter((menu) => !['Logout'].includes(menu[1].name))
+    .map((menu) => (
+      <Link href={menu[1].url} key={menu[1].name}>
+        <Box
+          as="a"
+          p={3}
+          borderRadius="lg"
+          cursor="pointer"
+          bgColor={
+            router.pathname === menu[1].url
+              ? 'rgba(0, 0, 0, 0.2)'
+              : 'transparent'
+          }
+        >
+          <Icon as={menu[1].icon} mr={4} />
+          <span>{menu[1].name}</span>
+        </Box>
+      </Link>
+    ));
 
   return (
     <Flex>
       <Flex
         h="100vh"
         flexBasis="300px"
-        bgGradient="linear(to-b, #0575E6, #021B79)"
-        color="white"
-        p={8}
         direction="column"
-        justify="center"
+        bgGradient="linear(to-b, #0575E6, #021B79)"
+        justifyContent="space-between"
+        p={8}
+        color="white"
       >
-        {listOfMenu}
+        <Link href={routes.admin.logout.url}>
+          <Box
+            as="a"
+            p={3}
+            borderRadius="lg"
+            cursor="pointer"
+            fontSize="xl"
+            fontWeight="bold"
+          >
+            HOLO
+          </Box>
+        </Link>
+        <Flex direction="column" justify="center">
+          {listOfMenu}
+        </Flex>
+        <Link href={routes.admin.logout.url}>
+          <Box as="a" p={3} borderRadius="lg" cursor="pointer">
+            <Icon as={routes.admin.logout.icon} mr={4} />
+            <span>{routes.admin.logout.name}</span>
+          </Box>
+        </Link>
       </Flex>
       <Box p={12} flexBasis="calc(100% - 300px)" h="100vh" overflowY="auto">
         <Flex
