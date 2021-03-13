9c3894d051eec63fec69725aa97ad9e8a3381a78
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-13 20:47:38 +0800

    add icon to routes

--- utils/routes.tsx
+++ utils/routes.tsx
@@ -1,9 +1,16 @@
+import { IoHandRight } from 'react-icons/io5';
+import {
+  RiDashboard3Line,
+  RiDashboardFill,
+  RiLogoutBoxRLine,
+} from 'react-icons/ri';
+
 export const routes = {
   admin: {
-    dashboard: { url: '/admin', name: 'Dashboard' },
-    intro: { url: '/admin/intro', name: 'Intro' },
-    items: { url: '/admin/items', name: 'Items' },
-    logout: { url: '/admin/logout', name: 'Logout' },
+    dashboard: { url: '/admin', name: 'Dashboard', icon: RiDashboard3Line },
+    intro: { url: '/admin/intro', name: 'Intro', icon: IoHandRight },
+    items: { url: '/admin/items', name: 'Items', icon: RiDashboardFill },
+    logout: { url: '/admin/logout', name: 'Logout', icon: RiLogoutBoxRLine },
   },
   client: {
     display: { url: '/display', name: 'Display' },
